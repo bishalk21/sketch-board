61fbace36f1aaee7a07c35b34bb157f73beeab6c
bishalk21 <karkibishal00@gmail.com>
2025-04-21 15:01:09 +1000

    some event actions added

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 let menuContainer = document.querySelector(".menu-container");
 let menuFlag = true;
+let toolsContainer = document.querySelector(".tools-container");
+let pencilToolContainer = document.querySelector(".pencil-tool-container");
+let eraserToolContainer = document.querySelector(".eraser-tool-container");
+let pencilTool = document.querySelector(".pencil");
+let eraserTool = document.querySelector(".eraser");
+let pencilFlag = false;
+let eraserFlag = false;
 
 // true -> show menu, false -> hide menu
 menuContainer.addEventListener("click", (e) => {
@@ -11,12 +18,30 @@ menuContainer.addEventListener("click", (e) => {
 function openMenu() {
   // let iconElement = document.querySelector(".menu-container i");
   let iconElement = menuContainer.children[0];
-  iconElement.classList.remove("fa-bars");
-  iconElement.classList.add("fa-xmark");
+  iconElement.classList.remove("fa-xmark");
+  iconElement.classList.add("fa-bars");
+  toolsContainer.style.display = "flex";
 }
 function closeMenu() {
   // let iconElement = document.querySelector(".menu-container i");
   let iconElement = menuContainer.children[0];
-  iconElement.classList.remove("fa-xmark");
-  iconElement.classList.add("fa-bars");
+  iconElement.classList.remove("fa-bars");
+  iconElement.classList.add("fa-xmark");
+  toolsContainer.style.display = "none";
+  pencilToolContainer.style.display = "none";
+  eraserToolContainer.style.display = "none";
 }
+
+// pencil tool
+pencilTool.addEventListener("click", (e) => {
+  pencilFlag = !pencilFlag;
+  if (pencilFlag) pencilToolContainer.style.display = "block";
+  else pencilToolContainer.style.display = "none";
+});
+
+// eraser tool
+eraserTool.addEventListener("click", (e) => {
+  eraserFlag = !eraserFlag;
+  if (eraserFlag) eraserToolContainer.style.display = "flex";
+  else eraserToolContainer.style.display = "none";
+});
